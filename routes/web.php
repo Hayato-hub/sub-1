887050c0feb217d91018b4bfb39ec5686f2d7221
EC2 Default User <ec2-user@ip-172-31-47-212.us-east-2.compute.internal>
2020-08-14 12:19:01 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 // LP獲得用
 Route::get('live', function () {
-    return view('google.check'); //確認用LP
-    // return view('google.noMatch'); //獲得LP(2)
+    // return view('google.check'); //確認用LP
+    return view('google.noMatch'); //獲得LP(2)
     
     // return view('google.live'); //獲得LP(1)
 });
