25c497591be93cd3fe1d2fccaf1f81e6013c0d0e
EC2 Default User <ec2-user@ip-172-31-47-212.us-east-2.compute.internal>
2020-09-14 20:44:16 +0000

    Mm

--- resources/views/google/noMatch.blade.php
+++ resources/views/google/noMatch.blade.php
@@ -16,10 +16,9 @@
         <div class="big-body">
             <div class="body-top">
                 <h1><span>AVに飽きた人必見!!!<br>ガチ素人の『裏配信』が今熱い！</span></h1>
-                <a href="https://preaf.jp/pa.do?s=v04031&o=46997&guid=ON" target="_blank"><img src="{{ asset('image/uraLive_1.JPG') }}"></a>
+                <a href="https://a-trade.jp/redirect/eazy?media=G16531" target="_blank"><img src="{{ asset('image/uraLive_1.JPG') }}"></a>
                 <h1><span>個人配信なら『見せ合い』も可能..!?</span></h1>
-                <a href="https://preaf.jp/pa.do?s=v04031&o=46997&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 裏配信はコチラ <i class="fas fa-external-link-alt"></i></a>
-                <!--<p>生配信のスクショ! IDで検索すれば過去配信閲覧できるでー</p>-->
+                <a href="https://a-trade.jp/redirect/eazy?media=G16531" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 裏配信はコチラ <i class="fas fa-external-link-alt"></i></a>
             </div>
             
             <div class="big-body-main">
@@ -29,58 +28,37 @@
                 </div>
                 
                 <div class="body-main-2">
-                    <img src="{{ asset('image/uraLive_3.JPG') }}">
+                    <img src="{{ asset('image/uraLive_2.JPG') }}">
                 </div>
                 
                 <div class="body-main-3">
                     <h3>指入れ・おもちゃ・見せ合い・鑑賞 etc...<br>ガチ裏垢女子が見つかるライブ配信</h3>
-                    <p>有名SNSを垢BANされただけあって、刺激強い配信が多数。Pornhubやdmm(Fanza)が飽きた人、リアルな素人のエロ動画を見たい人は必見。<br><br>オナニーだけでも種類は豊富。指入れやクリトリス、おもちゃプレイは勿論、個人配信で繋がればあなたがオナニー指示を出してもOK。</p>
+                    <p>有名SNSを垢BANされただけあって、刺激強い配信が多数。Pornhubやdmm(Fanza)が飽きた人、リアルな素人のエロ動画を見たい人は必見。<br><br>オナニーだけでも種類は豊富。指入れやクリトリス、おもちゃプレイは勿論、個人配信で繋がれば<span class="red">あなたがオナニー指示を出してもOK</span>。</p>
+                    <img src="{{ asset('image/uraLive_3.JPG') }}">
                     <br>
                     <h3>性欲強め女子多数なので<br>SNS交換して実際にオフパコするケースも..!?</h3>
-                    <p>中にはTwitterIDなどを公開してるライバーさんもおり、定期的にオフ会を開催しているケースもあります。<br><br>オフ会でなにをするかは言わずもがな。</p>
+                    <p>中にはTwitterIDなどを公開してるライバーさんもおり、<span class="red">定期的にオフ会を開催</span>しているケースもあります。<br><br>オフ会でなにをするかは言わずもがな。</p>
                     <br>
+                    <h3>ライブ配信を保存してると...<br>データフォルダに要注意！</h3>
+                    <p>アーカイブ機能は無いので、貴重なライブ配信は保存して見逃すな。</p>
                     <img src="{{ asset('image/uraLive_4.JPG') }}">
-                    <h3>最近使い過ぎてデータフォルダがやばい件wwwww</h3>
-                    <p>オナ電の動画保存してたらまじでこうなるから気をつけてwww</p>
-                    <!--<br>-->
-                    <!--<h3>使い方は簡単！TwitterとかYouTube使えてたら分かるはず！</h3>-->
-                    <!--<p>TwitterみたいにIDで検索して女の子探したり、YouTubeみたいにキーワード検索してライブ配信や過去配信見たりって感じ！まー若者なら分かるっしょw</p>-->
-                    <!--<br>-->
-                    <h3>でも、やっぱり実際会うのが一番！<br>全国各地から集ってる感じだしほぼほぼ会えるの間違いなし！</h3>
-                    <!--<h3>ただ...いつ規制されてサービス停止されるか分からない！</h3>-->
-                    <p>ただ...ぶっちゃけまじで無法地帯だからいつ規制されてもおかしくない！でもまー立ち位置的には普通のSNSだし使うにあたって身分提示するような事もないし、何かあっても身バレしないのはデカイかな！</p>
-                    <!--<img src="{{ asset('image/uraLive_5.jpg') }}">-->
-                    <!--<br>-->
-                    <!--<br>-->
-                    <h3>リンク切れになってたらスマン！！まだリンク生きてたら削除前に急げよ！！！</h3>
-                    <a href="https://preaf.jp/pa.do?s=v04031&o=46997&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
-                    <!--<a href="https://preaf.jp/pa.do?s=v04031&o=46959&guid=ON" target="_blank"><p><span>https://apps.apple.com/jp/app/free/<br>tiktok-ura/goody</p></span></p></a>-->
-                    <img src="{{ asset('image/uraLive_ura.jpg') }}">
-                    <p>↑これな！</p>
                     <br>
-                    <!--<p>最後にちょっと宣伝！Twitterで裏系・エロ情報発信してこうと思うから、気になるやつはフォローしてやー！@adult_info_</p>-->
-                    <!--<br>-->
-                    <!--<div class="line"></div>-->
-                    <!--<br>-->
-                    <img src="{{ asset('image/uraLive_1.JPG') }}">
-                    <img src="{{ asset('image/uraLive_2.JPG') }}">
-                    <img src="{{ asset('image/uraLive_3.JPG') }}">
-                    <img src="{{ asset('image/uraLive_4.JPG') }}">
+                    <h3>運が良ければ<br>ガチ素人の生ハメ配信観れるかも..!?</h3>
+                    <p>稀にある生ハメ配信。この配信はなんと<span class="red">同時視聴者数6000人以上</span>を記録。</p>
                     <img src="{{ asset('image/uraLive_5.jpg') }}">
+                    <br>
+                    <h3>いつアプリ規制されるか分からない<br>サービス停止前に入っていつでも戻れるようにしよう</h3>
+                    <p>これだけの裏サービス。いつ規制されてアプリストアから消されるか分かりません。<br>ただ、<span class="red">一度インストールしてるといつでも戻ってこれる</span>のでまずはインストールだけでもオススメです。</p>
+                    <div class="center">
+                        <a href="https://a-trade.jp/redirect/eazy?media=G16531" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 裏配信はコチラ <i class="fas fa-external-link-alt"></i></a>
+                    </div>
+                    <a href="https://a-trade.jp/redirect/eazy?media=G16531" target="_blank"><img src="https://trading-ad.net/a-trade/banner/eazy_640x200_syogekinoshiyo01_ss50_main.gif" class="wide"></a>
+                    <br>
                 </div>
             </div>
-            
-            <!--<div class="ad">-->
-            <!--    <h1><span>【PR】時給1000円以上! アダルトゲームのテスター募集!</span></h1>-->
-                <!--<a href="https://a-trade.jp/redirect/nijikano?media=G16531" target="_blank"><img src="https://trading-ad.net/a-trade/banner/niji_600x500_001.jpg"></a>-->
-            <!--    <a href="https://a-trade.jp/redirect/anahiro?media=G16531" target="_blank"><img src="https://trading-ad.net/a-trade/banner/ana_600x500_009.jpg" alt=""></a>-->
-            <!--    <p><span><i class="far fa-hand-point-up"></i> <i class="far fa-hand-point-up"></i> <i class="far fa-hand-point-up"></i></span></p>-->
-            <!--    <p><span>まず普通にユーザー登録して詳細を確認せよ！</span></p>-->
-            <!--</div>-->
-            <!--<br>-->
         </div>
         <footer>
-            <p>激ヤバ話題まとめ@5ch</p>
+            <p>裏垢女子まとめ@5ch</p>
         </footer>
     </body>
 </html>
