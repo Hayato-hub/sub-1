43318276710177a948e37afecd59fe22c533b373
EC2 Default User <ec2-user@ip-172-31-47-212.us-east-2.compute.internal>
2020-09-14 19:59:17 +0000

    mM

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,18 @@
 */
 
 // LP獲得用
-Route::get('new', function () {
+Route::get('app', function () {
     return view('google.check'); //確認用LP
-    // return view('google.mama'); //獲得LP(3)
-    
     // return view('google.noMatch'); //獲得LP(2)
+    
+    // return view('google.mama'); //獲得LP(3)
     // return view('google.live'); //獲得LP(1)
 });
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('google.mama'); //獲得LP(3)
+    return view('google.noMatch'); //獲得LP(2)
     
-    // return view('google.noMatch'); //獲得LP(2)
+    // return view('google.mama'); //獲得LP(3)
     // return view('google.live'); //獲得LP(1)
 });
